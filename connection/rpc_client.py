fe48d79de9891075c6faedd3a67642d1211bb89f
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2019-09-02 01:04:52 -0300

    Fix which the object reference in name server

--- connection/rpc_client.py
+++ connection/rpc_client.py
@@ -1,15 +1,15 @@
 import Pyro4
 import threading
 
-SERVER_NAME = "PYRONAME:server"
-CLIENT_NAME = "PYRONAME:client"
+SERVER_NAME = "PYRONAME:checkers.server"
+CLIENT_NAME = "PYRONAME:checkers.client"
 
 
 @Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class Client(object):
-    def __init__(self, game):
+    def __init__(self):
         self.daemon = Pyro4.Daemon()
-        self.game = game
         self.server_name = Pyro4.locateNS()
 
         self.client_type = ''
@@ -18,21 +18,24 @@ class Client(object):
 
         self.enemy = None
 
-    def register_on_server_name(self, register_name):
-        uri = self.daemon.register(Client)
+    def register_on_server_name(self, register_name, client_object):
+        uri = self.daemon.register(client_object)
         self.server_name.register(register_name, uri)
 
         thread = threading.Thread(target=self.daemon.requestLoop)
         thread.start()
 
-    def start_connection(self):
+    def start_connection(self, client_object):
         try:
-            ns = self.server_name.lookup(SERVER_NAME)
-            self.register_on_server_name(CLIENT_NAME)
+            ns = self.server_name.lookup("checkers.server")
+            self.register_on_server_name("checkers.client", client_object)
             self.enemy = Pyro4.Proxy(SERVER_NAME)
+            self.status_text = 'Conectado'
+            self.status_type = 1
+            self.client_type = 'client'
         except:
-            self.register_on_server_name(SERVER_NAME)
+            self.register_on_server_name("checkers.server", client_object)
             self.enemy = Pyro4.Proxy(CLIENT_NAME)
-
-    def teste(self):
-        print('teste')
+            self.status_text = 'Conectado'
+            self.status_type = 1
+            self.client_type = 'server'
--- connection/rpc_game_client.py
+++ connection/rpc_game_client.py
@@ -5,14 +5,16 @@ from utils.utils import import_file
 WHITE = (255, 255, 255)
 
 
-class GameClient(Client):
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
+class GameClient(Client, object):
     def __init__(self):
-        Client.__init__(self, GameClient)
+        Client.__init__(self)
         self.cells = []
         self.messages = []
         self.client_type_turn = 'server'
 
-    def __update_cells(self, id_origin, id_destiny):
+    def update_cells(self, id_origin, id_destiny):
         color_origin = self.cells[id_origin].color
         color_destiny = self.cells[id_destiny].color
 
@@ -21,24 +23,24 @@ class GameClient(Client):
 
     def move_cell(self, id_origin, id_destiny):
         self.client_type_turn = 'server' if self.client_type == 'client' else 'client'
-        self.__update_cells(id_origin, id_destiny)
-        self.enemy.__update_cells(id_origin, id_destiny)
+        self.update_cells(id_origin, id_destiny)
+        self.enemy.update_cells(id_origin, id_destiny)
 
-    def __new_message(self, message):
+    def new_message(self, message):
         self.messages.append(message)
 
     def send_message(self, message):
-        self.__new_message(message)
-        self.enemy.__new_message(message)
+        self.new_message(message)
+        self.enemy.new_message(message)
 
-    def __on_restart_game(self):
+    def on_restart_game(self):
         cell_list = import_file('table_cells.json')
         for cell in cell_list:
             self.cells[cell['id']].color = eval(cell['default_color'])
 
     def restart_game(self):
-        self.__on_restart_game()
-        self.enemy.__on_restart_game()
+        self.on_restart_game()
+        self.enemy.on_restart_game()
 
     def __on_close(self):
         self.status_text = 'Adversário desistiu!'
@@ -47,4 +49,3 @@ class GameClient(Client):
     def on_close(self):
         self.enemy.__on_close()
         # self.close()
-
